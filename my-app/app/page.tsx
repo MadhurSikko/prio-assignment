0d62a848b808700672c3c17754fe80c4999b38f4
Madhur <madhur.dagar.s@gmail.com>
2024-11-20 17:10:54 +0530

    redirect changes

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,4 +2,9 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect('/agents')
+  return (
+    <>
+      This is the home page
+    </>
+  )
 }
--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -4,4 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  redirects: async () => [
+    {
+      source: '/',
+      destination: '/agents',
+      permanent: true, // Use false for a 302 redirect
+    },
+  ],
+};
+
 export default nextConfig;
