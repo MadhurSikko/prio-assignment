82f68795b4e8e32a4cde1eedea85fa3bb9f4d823
Madhur <madhur.dagar.s@gmail.com>
2024-11-21 15:09:22 +0530

    some changes

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -4,17 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-module.exports = {
-  redirects: async () => [
-    {
-      source: '/',
-      destination: '/agents',
-      permanent: true, // Use false for a 302 redirect
-    },
-  ],
-  env: {
-    token: process.env.token,
-  },
-};
 
 export default nextConfig;
